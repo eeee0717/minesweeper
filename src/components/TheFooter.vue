8f4cc7cfe278b6f75e4dcfb1027cfe537faacbf1
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-10-20 21:51:34 +0800

    commit

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/eeee0717"
       target="_blank"
       title="GitHub"
     />
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
